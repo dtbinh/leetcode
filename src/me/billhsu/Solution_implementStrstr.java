089d39d44771023dd2e08615d3a3b8e30a5d12b2
Shipeng Xu <BillHsu.x@gmail.com>
2015-02-19 15:27:02 +0800

    Implement strStr()

---
+++ src/me/billhsu/Solution_implementStrstr.java
@@ -0,0 +1,11 @@
+package me.billhsu;
+
+/**
+ * Created by Shipeng Xu on 2/19/15.
+ * https://oj.leetcode.com/problems/implement-strstr/
+ */
+public class Solution_implementStrstr {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
