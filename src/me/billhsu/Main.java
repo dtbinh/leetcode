3b52f72559090698c0b4c64c47c50f41baef5447
Shipeng Xu <billhsu.x@gmail.com>
2015-07-05 14:59:35 +0800

    remove Main.java

--- src/me/billhsu/Main.java
+++ src/me/billhsu/Main.java
@@ -1,26 +1,24 @@
 package me.billhsu;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
-
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
         ListNode l2 = new ListNode(2);
         ListNode l3 = new ListNode(3);
-        ListNode l4 = new ListNode(4);
+        ListNode l4 = new ListNode(2);
         ListNode l5 = new ListNode(5);
-        ListNode l6 = new ListNode(6);
+        ListNode l6 = new ListNode(2);
+        ListNode l7 = new ListNode(5);
 
         l1.next = l2;
         l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-        l5.next = l6;
-
-        int[][] input = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
-        new Solution_searchA2dMatrix().searchMatrix(input, 3);
+//        l3.next = l4;
+//        l4.next = l5;
+//        l5.next = l6;
+//        l6.next = l7;
+        TreeNode root = new TreeNode(0);
+        TreeNode left = new TreeNode(1);
+        root.left = left;
+        new Solution_recoverBinarySearchTree().recoverTree(root);
     }
 }
